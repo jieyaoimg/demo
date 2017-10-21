fde0b02d4a239dbe1621f91ff49de0a61a498a5e
jieyao <jieyao.jieyao.com>
2017-10-21 20:11:40 +0800

    test01

--- vue/my-project/src/components/ttttt.js
+++
@@ -1,8 +0,0 @@
-/node_modules/  //只忽略根目录下的node_modules目录
-node_modules/  //忽略项目里的所有node_modules目录，不管是根目录下还是子目录下
-*.suo  //忽略所有.suo格式的文件
-.idea  //忽略所有.idea格式的文件
-/*   //忽略全部内容
-!.gitignore  //不忽略.gitignore文件
-!/fw/bin/   不忽略根目录下的/fw/bin/目录
-# Editor directories and files  //#号表示注释
