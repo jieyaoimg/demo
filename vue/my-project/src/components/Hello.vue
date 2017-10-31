5167db621485643fbc57563750b217007fc29e4f
jieyao <jieyao.jieyao.com>
2017-10-31 18:26:36 +0800

    html5-event

--- vue/my-project/src/components/Hello.vue
+++ vue/my-project/src/components/Hello.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <progress></progress>
+    <progress value=".4"></progress>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
+  name: "hello",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
