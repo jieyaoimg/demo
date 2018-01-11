87d4aaa20d73774cbf6a2203500d5fab11f39234
jieyao <jieyao.jieyao.com>
2018-01-11 16:31:09 +0800

    del

--- webpack/webpack-MultiPage-frame/postcss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  plugins: [    //PostCSS的一些插件
-      require('autoprefixer')
-  ]
-}
--- webpack/webpack-MultiPage-frame/src/js/components/nav/nav.js
+++
@@ -1,6 +0,0 @@
-const navHtml=require('./template/nav.html')
-const navScss=require('./css/nav.scss')
-module.exports=function(){
-  /* eslint-disable no-undef */
-  $('body').prepend(navHtml)
-}
